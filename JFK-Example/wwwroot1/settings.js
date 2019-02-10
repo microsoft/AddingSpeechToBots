094fb5a8de20e69522bb07d0a250af1f253fb537
Chris Hoder <chrhoder@microsoft.com>
2019-02-10 07:11:03 -0800

    addeding JFK Hoover bot & template files

---
+++ JFK-Example/wwwroot1/settings.js
@@ -0,0 +1,8 @@
+// Bot settings
+const botSecret = "from the Direct Line channel in the Channels blade of your Web App Bot";
+
+// Speech settings
+const speechKey = "from the Keys blade of your Speech Service instance in the Azure portal";
+const speechRecognitionEndpoint = "10-minute wss URL from the Endpoints page of the Custom Speech portal";
+const speechSynthesisEndpoint = "from the Endpoints page of the Custom Voice portal";
+const tokenEndpoint = "https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken";

82d4445c2c0ed0ad4f4fbfceca9d9fd0a5e0bdd4
Jerry Kindall <jkindall@aquent.com>
2019-02-11 11:36:21 -0800

    move speech region to settings.js

--- JFK-Example/wwwroot1/settings.js
+++ JFK-Example/wwwroot1/settings.js
@@ -3,6 +3,7 @@ const botSecret = "from the Direct Line channel in the Channels blade of your We
 
 // Speech settings
 const speechKey = "from the Keys blade of your Speech Service instance in the Azure portal";
+const speechRegion = "westus";
 const speechRecognitionEndpoint = "10-minute wss URL from the Endpoints page of the Custom Speech portal";
 const speechSynthesisEndpoint = "from the Endpoints page of the Custom Voice portal";
 const tokenEndpoint = "https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken";
